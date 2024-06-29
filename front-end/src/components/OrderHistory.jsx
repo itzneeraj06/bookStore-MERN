2d4cd9485fd9d05e020ba5fc1ded1c4d51a20245
Neeraj Chouhan <136504423+itzneeraj06@users.noreply.github.com>
2024-06-30 00:03:46 +0530

    add orderhistory

--- front-end/src/components/OrderHistory.jsx
+++ front-end/src/components/OrderHistory.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useEffect,useState } from 'react'
+import Loader from './Loader'
+import axios from 'axios'
 const OrderHistory = () => {
+  const [orderhistory, setorderhistory] = useState()
+  const headers = {
+    id: localStorage.getItem("id"),
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  }
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get("http://localhost:4000/api/v1/orderhistory", { headers });
+      setorderhistory(response.data.data);
+      console.log(response.data.data);
+
+    };
+    fetch();
+
+
+  },[]);
   return (
-    <div className=''>OrderHistory</div>
+    <div className=''>
+
+    </div>
   )
 }
 
