87253d060d0b9e293462a4bf25f6b5d98dc51bb7
Neeraj Chouhan <136504423+itzneeraj06@users.noreply.github.com>
2024-07-10 10:39:38 +0000

    trending add

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Recently from '../components/Recently'
+import Trending from '../components/Trending'
 
 const Home = () => {
   return (
     <div className="bg-zinc-900">
       <Hero />
-      <Recently/>
+      <Trending />
+      <Recently />
     </div>
   )
 }
---
+++ front-end/src/pages/Trending.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Trending() {
+    const [data, setData] = useState();
+    let trending;
+    if (data) {
+        trending = data[Math.floor(Math.random() * data.length)];
+    }
+    const fetch = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/getallbooks`)
+        setData(response.data.data);
+    }
+    useEffect(() => {
+        fetch();
+    }, [])
+
+    return (
+        <div className=' bg-zinc-800 p-5 my-5 mx-auto w-[50%] rounded'>
+            <p className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-left lg:text tracking-widest flex items-center mb-8' >Trending Book</p>
+
+
+            {
+                data &&
+                <div className=''>
+                    <Link to={`/getdetails/${trending._id}`}>
+                        <div className=''>
+                            <div className=''>
+                                <img src={trending.url} alt="" className='h-[45vh] rounded m-auto ' />
+                            </div>
+                            <div>
+                                <p className='text-zinc-400 text-center'>( {trending.title} )</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            }
+
+        </div>
+    );
+}
+
+export default Trending;
