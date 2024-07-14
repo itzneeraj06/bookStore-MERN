d75d906de2f8ad71e3ecb56949a99da34a1da472
Neeraj Chouhan <136504423+itzneeraj06@users.noreply.github.com>
2024-07-14 22:02:55 +0530

    add responsiveness on Trending

--- front-end/src/components/Trending.jsx
+++ front-end/src/components/Trending.jsx
@@ -17,16 +17,17 @@ function Trending() {
     }, [])
 
     return (
-        <div className=' bg-zinc-800 p-5 my-5 mx-auto w-[50%] rounded'>
-            <p className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-left lg:text tracking-widest flex items-center mb-8' >Trending Book</p>
+        <div className=' bg-zinc-800 p-5 my-5 mx-auto sm:w-[50%] rounded'>
+
 
 
             {
                 data &&
-                <div className=''>
+                <div>
+                    <p className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-left lg:text tracking-widest flex items-center mb-8' >Trending Book</p>
                     <Link to={`/getdetails/${trending._id}`}>
-                        <div className=''>
-                            <div className=''>
+                        <div>
+                            <div>
                                 <img src={trending.url} alt="" className='h-[45vh] rounded m-auto ' />
                             </div>
                             <div>
